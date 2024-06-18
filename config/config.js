fd5853a28b9d6fd75eb53088c12f2d5dae3150e5
dhanu0510 <dhananjaypatel.official+it@gmail.com>
2024-06-18 19:40:42 -0400

    for production

--- config/config.js
+++ config/config.js
@@ -16,20 +16,6 @@ const config = {
     port: process.env.DB_PORT,
     dialect: "postgres",
   },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
 };
 
 module.exports = config;
--- config/database.js
+++ config/database.js
@@ -3,9 +3,6 @@ const { Sequelize } = require("sequelize");
 const config = require("./config");
 
 let mode = "development";
-if (process.env.NODE_ENV === "production") {
-  mode = "production";
-}
 const sequelize = new Sequelize(config[mode]);
 
 module.exports = sequelize;
